6a29dacaff5f2160ecd50c8678a586afc088268c
kong <type5617@163.com>
2021-05-28 03:51:12 +0800

    新增端口和全局路径

--- src/main/java/com/kong/wiki/WikiApplication.java
+++ src/main/java/com/kong/wiki/WikiApplication.java
@@ -1,13 +1,23 @@
 package com.kong.wiki;
 
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WikiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
-    }
+    private static final Logger log = LoggerFactory.getLogger(WikiApplication.class);
 
+    public static void main(String[] args) {
+
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+
+        // 配置一下启动日志的提示
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功！！");
+        log.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
 }
