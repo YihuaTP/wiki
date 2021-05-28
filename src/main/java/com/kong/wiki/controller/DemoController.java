e16bcbe7e5fdf43f2f17476253393433b2805d88
kong <type5617@163.com>
2021-05-28 16:32:22 +0800

    一些常规配置

--- src/main/java/com/kong/wiki/controller/DemoController.java
+++ src/main/java/com/kong/wiki/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.kong.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,10 @@ public class DemoController {
     @GetMapping(value = "/get")
     public String getDemo() {
         return "getDemo";
+    }
+
+    @PostMapping(value = "/post")
+    public String postDemo(String name) {
+        return "postDemo" + name;
     }
 }
