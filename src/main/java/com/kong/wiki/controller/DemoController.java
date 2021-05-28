9e881301a920034bd87e6688509613ce15726448
kong <type5617@163.com>
2021-05-28 13:07:08 +0800

    使用idea自带的http测试接口

--- src/main/java/com/kong/wiki/controller/DemoController.java
+++ src/main/java/com/kong/wiki/controller/DemoController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 测试controller
+ */
 @RestController
 @RequestMapping(value = "/demo")
 public class DemoController {
